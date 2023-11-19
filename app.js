a285f24e19199023b14640c999f07985bbc64647
Mocus <mocussmncube@gmail.com>
2023-11-19 07:59:53 +0200

    added the grid onto my game using js - created the the createGameBoard()

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 const cards = [
     { name: 'Fries',
-      img: "",
-
+      img:"assets/fries.png" ,
+    },
+     { name: 'Blank',
+      img:"assets/blank.png" ,
+    },
+     { name: 'Cheeseburger',
+      img:"assets/cheeseburger.png" ,
+    },
+     { name: 'Hotdog',
+      img:"assets/hotdog.png" ,
+    },
+    { name: 'Milkshake',
+      img:"assets/milkshake.png" ,
+    },
+     { name: 'Pizza',
+      img:"assets/pizza.png" ,
+    },
+      { name: 'Fries',
+      img:"assets/fries.png" ,
+    },
+     { name: 'Blank',
+      img:"assets/blank.png" ,
+    },
+     { name: 'Cheeseburger',
+      img:"assets/cheeseburger.png" ,
+    },
+     { name: 'Hotdog',
+      img:"assets/hotdog.png" ,
+    },
+    { name: 'Milkshake',
+      img:"assets/milkshake.png" ,
+    },
+     { name: 'Pizza',
+      img:"assets/pizza.png" ,
     }
 ]
+
+cards.sort(()=> 0.5 - Math.random());
+
+
+const gridDisplay = document.querySelector("#grid");
+
+function createBoard() {
+     for (let i = 0; i < cards.length; i++) {
+          const card = document.createElement('img');
+          card.setAttribute('src','assets/blank.png');
+          card.setAttribute('data-id',i);
+          gridDisplay.appendChild(card)
+     }
+}
+
+createBoard();
